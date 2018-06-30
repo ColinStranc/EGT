5a96fd6a902991a866e663c0f351d851f2b7bca7
Justin D'Astous <jdastous@edu.uwaterloo.ca>
2018-06-30 17:08:12 -0400

    Add class for public goods games
    This class will handle all the public goods games.
    
    Added function that will determine agent cooperation based on neighbours

---
+++ modelling/public_goods_games.py
@@ -0,0 +1,24 @@
+import modelling.agent as g
+import modelling.constants.cooperation_strategies as coop_strat
+import modelling.constants.punishement_strategies as punish_strat
+
+
+class public_goods_games
+
+    def get_agent_cooperation(agent, neighbours):
+        if g.Agent.get_cooperation_strategy(agent) == coop_start.COOPERATOR
+            return -1
+        else if g.Agent.get_cooperation_strategy(agent) == coop_start.DEFECTING
+            return 0
+        else
+            responsible_punishers = 0
+            anti_social_punishers = 0
+            for neighbour in neighbours
+                if g.Agent.get_punishment_strategy(neighbour) == punish_strat.RESPONSIBLE
+                    responsible_punishers++
+                else if g.Agent.get_punishment_strategy(neighbour) == punish_strat.ANTI_SOCIAL
+                    anti_social_punishers++
+            if responsible_punishers > anti_social_punishers
+                return -1
+            else
+                return 0
