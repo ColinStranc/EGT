1d9ce4598f027988e12883f0507023d42d8c7d91
AMoore202 <alexandermoore202@gmail.com>
2019-01-13 14:28:28 -0500

    Combined the cstrat and pstrat formulas
    Combined the analysis and plot functions for contribution and
    punishment strategies.

--- analysis/Analysis.R
+++ analysis/Analysis.R
@@ -4,7 +4,6 @@ library(dplyr)
 library(data.table)
 library(ggplot2)
 
-
 # Read data
 source(file.path("./read_egt_2.R"),chdir=F)
 # FileThreat03Trial01 <- read.egt("../modelling/storage/egt_02_23_1841.egt")
@@ -66,7 +65,6 @@ ThreatLevelVector <- c(3,25)
 LengthThreatLevelVector <- length(ThreatLevelVector)
 ThreatLevelIndex <- c(1:LengthThreatLevelVector)
 
-# Function used in both other functions to summarize the means and standard deviations of the second row of a dataframe.
 SummaryFunction <- function(DF){
   Summary <- c(Proportion=mean(DF[[2]]),sd=sd(DF[[2]]))
   return(Summary)
@@ -75,6 +73,8 @@ SummaryFunction <- function(DF){
 StratDataPrep <- function(ThreatList,Levels,Strategy){
   StratThreatLevelAnalysis <- function(TrialList){
     StratProportionFunction <- function(Trial){
+      Trial[[4]] <- recode(Trial[[4]],'1'='Contribute','2'='Dissent','3'='Opportunistic')
+      Trial[[5]] <- recode(Trial[[5]],'1'='Responsibly','2'='Anti_Socially','3'='Spitefully','4'='Never')
       NRow <- nrow(Trial)
       Counts <- ddply(Trial,Strategy,nrow)
       Counts[2] <- Counts[2] / NRow
@@ -98,16 +98,16 @@ StratDataPrep <- function(ThreatList,Levels,Strategy){
   return(SimulationDF)
 }
 
-CStratAnalysis <- function(SimDF){
+StratAnalysis <- function(SimDF){
   SimDF$Threat_Level <- as.factor(SimDF$Threat_Level)
   ANOVAFunction <- function(DF){
-    ANOVA <- aov(Proportion~Threat_Level,data=DF)
+    ANOVA <- aov(Proportion~Threat_Level,DF)
     return(ANOVA)
   }
-  ANOVAList <- dlply(SimDF,.(Contribution_Strategy),ANOVAFunction)
+  ANOVAList <- dlply(SimDF,"Strategy",ANOVAFunction)
   ANOVASummaryTableFunction <- function(i){
     Summary <- summary(ANOVAList[[i]])
-    SummaryTable <- data.frame(Contribution_Strategy=names(ANOVAList[i]),
+    SummaryTable <- data.frame(Strategy=names(ANOVAList[i]),
       DF_threat=Summary[[1]][1,1],
       DF_error=Summary[[1]][2,1],
       SS_threat=Summary[[1]][1,2],
@@ -127,84 +127,29 @@ CStratAnalysis <- function(SimDF){
     return(Tukey)
   }
   TukeyList <- lapply(ANOVAList,TukeyFunction)
-  names(TukeyList) <- c("Contribute","Dissent","Opportunistic","Percentage_Contributing")
   ResultsList <- list(ANOVASummaryTable,TukeyList) 
   return(ResultsList)
 }
 
-CStratPlot <- function(SimulationDF){
-  SummaryTable <- ddply(SimulationDF,.(Contribution_Strategy,Threat_Level),SummaryFunction) # Use the summary function to find the mean proportion and SD for each contribution strategy and threat level.
-  CStratPlot <- ggplot(data = SummaryTable, aes(x=Threat_Level, y=Proportion, group=Contribution_Strategy, colour=Contribution_Strategy)) +
+StratPlot <- function(SimulationDF,Strategy){
+  SummaryTable <- ddply(SimulationDF,.(Strategy,Threat_Level),SummaryFunction)
+  Labels <- if(Strategy=="Contribution"){c("Contribute", "Dissent", "Opportunistic","% Contributing")}else{c("Responsibly", "Anti-Socially", "Spitefully","Never")}
+  CStratPlot <- ggplot(data=SummaryTable,aes(x=Threat_Level,y=Proportion,group=Strategy,colour=Strategy)) +
     geom_line() +
-    geom_point(aes(shape=Contribution_Strategy),size=3) +
+    geom_point(aes(shape=Strategy),size=3) +
     scale_x_continuous(breaks=seq(min(ThreatLevelVector),max(ThreatLevelVector),ThreatLevelVector[2]-ThreatLevelVector[1])) +
-    coord_cartesian(ylim = c(0, 1)) +
+    coord_cartesian(ylim=c(0, 1)) +
     geom_errorbar(aes(ymin=Proportion-(sd/2), ymax=Proportion+(sd/2)), width=1,position=position_dodge(0.05)) +
-    labs(title="Contribution Strategy",x="Threat Level",y="Long-Term Avg. Population Proportion") +
-    scale_colour_discrete(name = "Contribution Strategy",breaks=c("Contribute", "Dissent", "Opportunistic","Percentage_Contributing"),labels = c("Contribute", "Dissent", "Opportunistic","% Contributing")) +
-    scale_shape_manual(name = "Contribution Strategy",breaks=c("Contribute", "Dissent", "Opportunistic","Percentage_Contributing"),labels = c("Contribute", "Dissent", "Opportunistic","% Contributing"),values = c(15:18)) +
+    labs(title=paste0(Strategy," Strategy"),x="Threat Level",y="Long-Term Avg. Population Proportion") +
+    scale_colour_discrete(name=paste0(Strategy," Strategy"),breaks=levels(SummaryTable$Strategy),labels=Labels) +
+    scale_shape_manual(name=paste0(Strategy," Strategy"),breaks=levels(SummaryTable$Strategy),labels=Labels,values=c(15:18)) +
     theme_light()
   return(CStratPlot)
 }
 
-
-PStratAnalysis <- function(SimDF){
-  SimDF$Threat_Level <- as.factor(SimDF$Threat_Level)
-  ANOVAFunction <- function(DF){
-    ANOVA <- aov(Proportion~Threat_Level,data=DF)
-    return(ANOVA)
-  }
-  ANOVAList <- dlply(SimDF,.(Punishment_Strategy),ANOVAFunction)
-  ANOVASummaryTableFunction <- function(i){
-    Summary <- summary(ANOVAList[[i]])
-    SummaryTable <- data.frame(Punishment_Strategy=names(ANOVAList[i]),
-      DF_threat=Summary[[1]][1,1],
-      DF_error=Summary[[1]][2,1],
-      SS_threat=Summary[[1]][1,2],
-      SS_error=Summary[[1]][2,2],
-      MS_threat=Summary[[1]][1,3],
-      MS_error=Summary[[1]][2,3],
-      F=Summary[[1]][1,4],
-      p=Summary[[1]][1,5],
-      EtaSq=Summary[[1]][1,2]/(Summary[[1]][1,2]+Summary[[1]][2,2])
-    )
-    return(SummaryTable)
-  }
-  ANOVASummaryTable <- ldply(c(1:length(ANOVAList)),ANOVASummaryTableFunction)
-  TukeyFunction <- function(ANOVA){
-    Tukey <- TukeyHSD(ANOVA)
-    Tukey <- Tukey$Threat_Level
-    return(Tukey)
-  }
-  TukeyList <- lapply(ANOVAList,TukeyFunction)
-  names(TukeyList) <- c("Responsibly","Anti_Socially","Spitefully","Never")
-  ResultsList <- list(ANOVASummaryTable,TukeyList) 
-  return(ResultsList)
-}
-
-PStratPlot <- function(SimulationDF){
-  SummaryTable <- ddply(SimulationDF,.(Punishment_Strategy,Threat_Level),SummaryFunction) # Use the summary function to find the mean proportion and SD for each punishment strategy and threat level.
-  PStratPlot <- ggplot(data = SummaryTable, aes(x=Threat_Level, y=Proportion, group=Punishment_Strategy, colour=Punishment_Strategy)) +
-    geom_line() +
-    geom_point(aes(shape=Punishment_Strategy),size=3) +
-    scale_x_continuous(breaks=seq(min(ThreatLevelVector),max(ThreatLevelVector),ThreatLevelVector[2]-ThreatLevelVector[1])) +
-    coord_cartesian(ylim = c(0, 1))+
-    geom_errorbar(aes(ymin=Proportion-(sd/2), ymax=Proportion+(sd/2)), width=1,position=position_dodge(0.05)) +
-    labs(title="Punishment Strategy",x="Threat Level",y="Long-Term Avg. Population Proportion") +
-    scale_colour_discrete(name = "Punishment Strategy",breaks=c("Responsibly", "Anti_Socially", "Spitefully","Never"),labels = c("Responsibly", "Anti-Socially", "Spitefully","Never")) +
-    scale_shape_manual(name = "Punishment Strategy",breaks=c("Responsibly", "Anti_Socially", "Spitefully","Never"),labels = c("Responsibly", "Anti-Socially", "Spitefully","Never"),values = c(15:18)) +
-    theme_light()
-  return(PStratPlot)
-}
-
-CStratData <- CStratDataPrep(ListOfThreats,ThreatLevelIndex)
-PStratData <- PStratDataPrep(ListOfThreats,ThreatLevelIndex)
-CStratAnalysis(CStratData)
-PStratAnalysis(PStratData)
-CStratPlot(CStratData)
-PStratPlot(PStratData)
-
-
-StrategyLists <- list(c('Contribute','Dissent','Opportunistic'),c("Responsibly","Anti_Socially","Spitefully","Never"))
-
-Counts[[1]] <- recode(Counts[[1]],'1'='Contribute','2'='Dissent','3'='Opportunistic')
+CStratData <- StratDataPrep(ListOfThreats,ThreatLevelIndex,"cstrats")
+PStratData <- StratDataPrep(ListOfThreats,ThreatLevelIndex,"pstrats")
+StratAnalysis(CStratData)
+StratAnalysis(PStratData)
+StratPlot(CStratData,"Contribution")
+StratPlot(PStratData,"Punishment")
